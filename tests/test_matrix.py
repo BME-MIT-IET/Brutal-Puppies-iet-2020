f178c6b2c3f0ce3c1905142a2e0eb0fc3fa19a04
Csinga24 <oszlanyi.csenge624@gmail.com>
2020-05-01 20:54:07 +0200

    add test top left invert matrix #1

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -63,6 +63,9 @@ class TestCopyTransform(unittest.TestCase):
         self.assertEqual(copy_transform.top_left_invert(
             [[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [[1, 4, 7], [2, 5, 8], [3, 6, 9]])
 
+        self.assertEqual(copy_transform.top_left_invert(
+            [[1, 4, 7], [2, 5, 8], [3, 6, 9]]), [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
         self.assertEqual(copy_transform.bottom_left_invert(
             [[1, 2, 3], [4, 5, 6], [7, 8, 9]]), [[9, 6, 3], [8, 5, 2], [7, 4, 1]])
 

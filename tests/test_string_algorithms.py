641304c95f434c7ab5e3fc579beb262384431c9a
feliciakranitz <felicia.kranitz@gmail.com>
2020-05-01 21:52:35 +0200

    is_anagram test fixed

--- tests/test_string_algorithms.py
+++ tests/test_string_algorithms.py
@@ -7,7 +7,7 @@ from algorithms.strings import *
 class TestIsAnagram(unittest.TestCase):
     def test_is_anagram(self):
         text = "golden retriever puppies"
-        this_is_an_anagram = "etrpepsi uprrvieer nedlog"
+        this_is_an_anagram = "etrpepsi uprvieer nedlog"
         this_is_not_an_anagram = "border collie puppies"
         self.assertTrue(is_anagram(text, this_is_an_anagram))
         self.assertFalse(is_anagram(text , this_is_not_an_anagram))
@@ -16,7 +16,6 @@ class TestIsPalindrome(unittest.TestCase):
     def test_is_palindrome(self):
         palindrome = "murder for a jar of red rum"
         not_palindrome = "this is not a palindrome :("
-        print(palindrome)
         self.assertFalse(is_palindrome(not_palindrome))
         self.assertTrue(is_palindrome(remove_punctuation(palindrome)))
 

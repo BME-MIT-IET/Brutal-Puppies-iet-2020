f99bff90fa37075640eb335f336388c0258e849d
feliciakranitz <felicia.kranitz@gmail.com>
2020-05-07 22:12:04 +0200

    not invariant returns fixed

--- algorithms/strings/make_sentence.py
+++ algorithms/strings/make_sentence.py
@@ -17,8 +17,6 @@ count = 0
 
 def make_sentence(str_piece, dictionaries):
     global count
-    if len(str_piece) == 0:
-        return True
     for i in range(0, len(str_piece)):
         prefix, suffix = str_piece[0:i], str_piece[i:]
         if prefix in dictionaries:
